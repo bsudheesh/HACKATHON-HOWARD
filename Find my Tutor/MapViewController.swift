3fd33cce666efc1037831ce18476e30e3da7eb1f
Sudheesh Bhattarai <sudheesh.bhattarai@bison.howard.edu>
2017-03-24 22:15:59 -0400

    Added map on the Home Page.

--- Find my Tutor/MapViewController.swift
+++ Find my Tutor/MapViewController.swift
@@ -7,13 +7,63 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
-    //@IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapView: MKMapView!
+    let manager = CLLocationManager()
+    
+    
+    //called everytime the user location is changed
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0] //all location are stored in this array, we get the recent location
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let myLocation: CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        //set the region
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span) //location and the span
+        //set the map
+        mapView.setRegion(region, animated: true)
+        
+        //add the blue dot
+        self.mapView.showsUserLocation = true
+ 
+        
+        /*
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: { (placemarks, error) -> Void in
+            if error != nil{
+                print("Error", error?.localizedDescription)
+                return
+            }
+            else if (placemarks?.count)! > 0{
+                let currentLocation = placemarks?[0]
+            }
+            
+            
+        })
+    */
+        
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error", error.localizedDescription)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        manager.delegate = self
+        
+        
+        manager.desiredAccuracy = kCLLocationAccuracyBest //get best accuracy
+        manager.requestWhenInUseAuthorization() //asking user's location in background
+        manager.startUpdatingLocation() //called when location is changed
         // Do any additional setup after loading the view.
     }
 
